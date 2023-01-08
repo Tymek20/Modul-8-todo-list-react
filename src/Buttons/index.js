864616f42b18c683815cf8a4497e54d0b883ff9e
Tomasz Szkudlarek <Tomasz.szkudlarek25@gmail.com>
2023-01-08 12:36:29 +0100

    merge button class of same css

--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -4,12 +4,16 @@ const Buttons = ({ tasks, hideDoneTasks }) => {
     if (tasks.lenght === 0) {
         return null;
     }
+
     return (
         <div className="button">
-            <button className="button__hide"> {hideDoneTasks ? "Pokaż " : "Ukryj "}
-                ukończone</button>
             <button
-                className="button__markDone"
+                className="button__buttons">
+                {hideDoneTasks ? "Pokaż " : "Ukryj "}
+                ukończone
+            </button>
+            <button
+                className="button__buttons"
                 disabled={tasks.every((task) => task.done)}>
                 Ukończ wszystkie
             </button>
