c5d288cd95d851d69dc679c2fed720526765ccc6
Tomasz Szkudlarek <Tomasz.szkudlarek25@gmail.com>
2023-02-23 18:50:06 +0100

    theme created

---
+++ src/theme.js
@@ -0,0 +1,21 @@
+export const theme = {
+    colors: {
+        primaryColor: "rgb(254, 255, 255)",
+        borderColor: "lightgray",
+        ebb: "#ebe0e0",
+        teal: "#008080",
+        white: "#ffffff",
+        japaneseLaurel: "#188005",
+        christi: "#21B007",
+        milanoRed: "#BC0606",
+        red: "#e60a0a",
+        lightRed: "#FF0000",
+        tallPoppy: "#B32B2B",
+        silver: "#ccc",
+        grey: "#808080",
+        fiord: "#3e5c6a",
+    },
+    breakpoint: {
+        mobile: 767,        
+    },
+};
